9e5331bb0dba3d5967ea8e5dee96b5cc9c4d2236
Jieun Lee <2ur.leeje@gmail.com>
2025-05-14 19:47:58 +0900

    deploy:#12 build 에러 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-5">
       <h1 className="border-b-2 border-yellow-900 justify-start text-2xl font-semibold text-amber-950">
-        TODAY's TO DO
+        TODAY&apos;s TO DO
       </h1>
       <p className="text-gray-500">Manage your life efficiently</p>
       <Link href={TODO_PAGE}>
