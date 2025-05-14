4738856dfacb8d92426afbb26708152f757c5d7e
Jieun Lee <2ur.leeje@gmail.com>
2025-05-14 19:28:07 +0900

    chore:#4 priority -> loading = "lazy"로 변경

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -9,7 +9,7 @@ const LoadingPage = () => {
         alt="스피너 svg 파일"
         height={80}
         width={80}
-        priority
+        loading="lazy"
       />
       <h1 className="font-bold text-3xl">Loading</h1>
     </section>
